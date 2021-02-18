8c81ced7d4c364f96038765c6edd4de5b3e54615
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-18 00:26:08 -0300

    Import Query Of TypeGraphQL

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,8 +1,14 @@
-import { Query } from "mongoose";
-import { Resolver } from "type-graphql";
+import { Query, Mutation, Resolver, InputType, Arg, Field } from "type-graphql";
 import Category from './Category'
 import CategorySchema from '../../models/CategorySchema'
 
+@InputType()
+class CategoryInput {
+    @Field()
+    description: String;
+    @Field()
+    name: String;
+}
 
 @Resolver(Category)
 class CategoryResolver {
@@ -10,6 +16,12 @@ class CategoryResolver {
     async categories() { // Parallel of name Query
         const categories = await CategorySchema.find()
         return categories
+    }
+
+    @Mutation(() => Category)
+    async createCategory(@Arg('categoryInput') categoryInput: CategoryInput) {
+        const category = await CategorySchema.create(categoryInput)
+        return category
     }
 }
 

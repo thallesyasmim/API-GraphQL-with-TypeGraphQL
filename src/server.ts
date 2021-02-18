c57a937ccb58e48c322f9a932ebe39579b3985df
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-18 00:27:04 -0300

    Import Video Resolver

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import 'reflect-metadata'
 import './utils/connection'
 import { ApolloServer } from 'apollo-server'
 import CategoryResolver from './graphql/category/CategoryResolver'
+import VideoResolver from './graphql/videos/VideoResolver'
 
 async function bootstrap() { // Initialization Function
     const schema = await buildSchema({
-        resolvers: [CategoryResolver]
+        resolvers: [CategoryResolver, VideoResolver]
     })
 
 

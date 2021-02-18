a724116c8e718c8f3dedf7e1c71ea82e074f049d
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-17 23:43:12 -0300

    Import Category Resolver

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import { buildSchema } from 'type-graphql'
 import 'reflect-metadata'
 import './utils/connection'
 import { ApolloServer } from 'apollo-server'
+import CategoryResolver from './graphql/category/CategoryResolver'
 
 async function bootstrap() { // Initialization Function
     const schema = await buildSchema({
-        resolvers: []
+        resolvers: [CategoryResolver]
     })
 
 
